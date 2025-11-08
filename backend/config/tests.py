2a60e21c3fafc0dd34162c736bdd5e15fd3497d2
pedrohpsantos <phps2014.phps@gmail.com>
2025-11-08 18:14:52 -0300

    fix(ci): unbreak lint stage

--- backend/config/tests.py
+++ backend/config/tests.py
@@ -3,5 +3,4 @@ from django.test import TestCase
 class SimpleTest(TestCase):
     def test_always_passes(self):
         """Um teste simples que sempre passa para o CI."""
-        x=1
         self.assertTrue(True)
